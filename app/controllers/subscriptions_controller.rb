5f25b5103cd6d76655cab29ec493d15acbf5aa17
Gavin Schreiber <gavin@s-aint.com>
2013-07-08 16:58:39 +0200

    rename controller

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,24 @@
+require "net/http"
+require "uri"
+
+class SubscriptionsController < ApplicationController
+ 
+  def index
+    @subscriber = ClearancePage.new
+  end
+
+
+  def subscribe
+    @subscriber = ClearancePage.new(params[:clearancepage])
+
+    if @subscriber.save
+      redirect_to root_path
+      flash[:notice] = 'You have succesfully entered the competition.'
+      else
+      render  :action => :index
+    end
+
+  end
+
+  
+end
