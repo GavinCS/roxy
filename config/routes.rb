2e845ea51176b9a53ea326dc7d2c7c3a2bc2afc2
Gavin Schreiber <gavin@s-aint.com>
2013-07-12 09:33:50 +0200

    form errors + msg

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Roxy::Application.routes.draw do
   
 
-#constraints :subdomain => "roxy" do
-#ActiveAdmin.routes(self)
-#  devise_for :admin_users, ActiveAdmin::Devise.config.merge(:path => '')
-#end
+constraints :subdomain => "roxy" do
+ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config.merge(:path => '')
+end
 
   get "clearancestore" , :to => "subscriptions#index"
   post "subscribe", :to => "subscriptions#subscribe"
